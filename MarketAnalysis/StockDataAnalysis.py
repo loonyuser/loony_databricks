1108ec19cf065a77c44038bd1627e3dd19b9aaea
cloud.user@loonycorn.com <cloud.user@loonycorn.com>
2022-07-09 12:56:56 +0000

    First check-in for stock analyzer

---
+++ MarketAnalysis/StockDataAnalysis.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+from pyspark.sql.functions import max
+
+# COMMAND ----------
+
+df = spark.read.format("delta")\
+          .load("dbfs:/user/hive/warehouse/tesla_stock_price")
+
+display(df)
+
+# COMMAND ----------
+
+df.select(max("Adj_Close"), max("Volume"))\
+  .withColumnRenamed("max(Adj_Close)", "Max_Close")\
+  .withColumnRenamed("max(Volume)", "Max_Volume")\
+  .show(truncate=False)
+
+# COMMAND ----------
+
+
